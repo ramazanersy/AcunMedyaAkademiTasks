9b4396bb21e9bd96f9f15fb3f51328878d45bbe4
ramazanersy <166340702+ramazanersy@users.noreply.github.com>
2025-02-28 00:38:08 +0300

    AcunMedyaAkademiTasks
    In this repository, the assignments given at the end of the training at Acunmedya Academy will be shared.

---
+++ ConsoleApp1/Classes/Araba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Classes
+{
+    public class Araba
+    {
+        public string marka;
+        public string vites;
+        public void vitesinigoster(string marka, string vites)
+        {
+            Console.WriteLine(marka + " " + vites);
+        }
+    }
+   
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1.Classes;
+
+Bmw bm=new Bmw();
+bm.marka = "BMW";
+bm.vites = "Düz Viteslidir";
+bm.vitesinigoster(bm.marka, bm.vites);
+
+Porsche po=new Porsche();
+po.marka = "Porsche";
+po.vites = "Otomatik Viteslidir";
+po.vitesinigoster(po.marka, po.vites);
+
+Mercedes me=new Mercedes();
+me.marka = "Mercedes";
+me.vites = "Otomatik Viteslidir";
+me.vitesinigoster(me.marka, me.vites);  
+
+Togg t=new Togg();
+t.marka = "Togg";
+t.vites = "Düz Viteslidir";
+t.vitesinigoster(t.marka, t.vites);
+
+Audi a=new Audi();
+a.marka = "Audi";
+a.vites = "Otomatik";
+a.vitesinigoster(a.marka, a.vites);
+
+Toyota to=new Toyota();
+to.vitesinigoster("Toyota", "Düz Viteslidir");
