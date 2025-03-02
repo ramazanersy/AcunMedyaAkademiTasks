32d767122a745e652d006f658f9966fb37e1dca5
ramazanersy <166340702+ramazanersy@users.noreply.github.com>
2025-03-02 20:16:00 +0300

    AbstractClassTask

---
+++ AbstractClassTask/AbstractClassTask/AbstractClassTask/Abstracts/Bmw.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassTask.Abstracts
+{
+    public class Bmw : Arac
+    {
+        public override double kaclitre()
+        {
+            return 80.0;
+        }
+    }
+}
---
+++ AbstractClassTask/AbstractClassTask/AbstractClassTask/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AbstractClassTask.Abstracts;
+
+Bmw bm=new Bmw();
+Mercedes me=new Mercedes();
+Porche po=new Porche();
+
+double toplamyakit = 0.0;
+toplamyakit += bm.kaclitre();
+toplamyakit += me.kaclitre();
+toplamyakit += po.kaclitre();
+Console.WriteLine("Toplam harcanan yakıt: " + "" + toplamyakit + " " + "Litredir.");
