fc7c11fa3255e0687e6835f905e9fcca64e69464
ramazanersy <166340702+ramazanersy@users.noreply.github.com>
2025-03-09 19:13:23 +0300

    Loop&ConditionAssignment

---
+++ Loop&conditionassigmnet/LoopAndCondition/LoopAndCondition/Insan.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAndCondition
+{
+    public class Insan
+    {
+         public int yasHesapla(int dogumyili)
+        {
+            int sonuc = 0;
+            string durum = "";
+            sonuc = DateTime.Now.Year - dogumyili;
+            if (sonuc > 0 && sonuc <= 18)
+            {
+                durum = "Küçüksünüz";
+            }
+            else if (sonuc > 18 && sonuc <= 35)
+            {
+                durum = "Gençsiniz";
+            }
+            else if (sonuc > 35 && sonuc <= 55)
+            {
+                durum = "Yetişkinsiniz";
+            }
+            else if (sonuc > 55 && sonuc <= 75)
+            {
+                durum = "Yaşlısınız";
+            }
+            else if (sonuc > 75 && sonuc <= 99)
+            {
+                durum = "Çok yaşlısınız";
+            }
+            else
+            {
+                durum = "Ya Hiç doğmadınız ya da çoktan öldünüz";
+            }
+            Console.WriteLine(durum);
+            return sonuc;
+           
+
+            
+        }
+    }
+}
---
+++ Loop&conditionassigmnet/LoopAndCondition/LoopAndCondition/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LoopAndCondition;
+
+Insan i=new Insan();
+i.yasHesapla(1925);
+
+Araba ar=new Araba();
+ar.DurumNe(2019);
