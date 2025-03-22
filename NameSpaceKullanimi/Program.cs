b8a3a1953b4f997fd501dcadac5ffd18fbd88267
ramazanersy <166340702+ramazanersy@users.noreply.github.com>
2025-03-22 13:19:29 +0300

    NameSpace

---
+++ NameSpaceKullanimi/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+/*
+ 1. "MatematikIslemleri" adında ProgramTest namespace'ine benzer bir namespace oluşturun.
+ 2. MatematikIslemleri namespace'inin içine "Hesaplama" adında bir sınıf oluşturun.
+ 3. Hesaplama sınıfı içine "Topla" ve "Çarp" adında iki metot oluşturun.
+    - Bu metotlar "sayi1" ve "sayi2" adında parametreler almalı.
+    - Topla metodu "sayi1 + sayi2" işlemini ekrana yazdırmalı.
+    - Çarpma metodu "sayi1 * sayi2" işlemini ekrana yazdırmalı.
+*/
+
+namespace MatematikIslemleri
+{
+    // Hesaplama sınıfı oluşturmanız isteniyor.
+
+    // Hesaplama sınıfı içerisine Topla ve Carp metotlarını ekleyiniz. 
+}
+
+// ProgramTest adında "MatematikIslemleri" namespace'i gibi yeni bir namespace oluşturun.
+namespace ProgramTest
+{
+    // Main metodu içeren sınıf
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MatematikIslemleri namespace'inden Hesaplama sınıfının nesnesini oluşturun.
+
+            // Yazdığınız metotları çağırın ve doğru çıktıları konsol ekranında görün.
+        }
+    }
+}
